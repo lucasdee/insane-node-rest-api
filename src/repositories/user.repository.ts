b90519d113847faa23922cefeb318bf134153f8a
lucasdee <lukasz.dolan@gmail.com>
2026-02-24 22:27:39 +0100

    feat: remove class-validator and use tsoa validation

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,20 +1,20 @@
 import { prisma } from '../config/db.js';
-import { User } from '../generated/prisma/client.js';
+import { UserCreateInput, UserModel } from '../generated/prisma/models.js';
 
 export class UserRepository {
-  async findByUsername(username: string): Promise<User | null> {
+  async findByUsername(username: string): Promise<UserModel | null> {
     return prisma.user.findUnique({
       where: { username },
     });
   }
 
-  async createUser(username: string, password: string, email: string, displayName: string | null = null): Promise<User> {
+  async createUser(data: UserCreateInput): Promise<UserModel> {
     return prisma.user.create({
-      data: { username, password, email, displayName },
+      data,
     });
   }
 
-  async findById(id: number): Promise<User | null> {
+  async findById(id: number): Promise<UserModel | null> {
     return prisma.user.findUnique({
       where: { id },
     });
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,17 +2,19 @@ import { env } from '../config/env.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { userRepository } from '../repositories/user.repository.js';
+import { LoginRequest, RegisterRequest } from '../dtos/auth.dto.js';
+import { User } from '../dtos/user.dto.js';
 
 export class UserService {
-  async register(username: string, password: string, email: string, displayName: string | null = null) {
+  async register({ username, password, email, displayName }: RegisterRequest): Promise<User> {
     const existing = await userRepository.findByUsername(username);
     if (existing) throw new Error('User already exists');
 
     const hashed = await bcrypt.hash(password, 10);
-    return userRepository.createUser(username, hashed, email, displayName);
+    return userRepository.createUser({username, password: hashed, email, displayName});
   }
 
-  async login(username: string, password: string) {
+  async login({ username, password }: LoginRequest): Promise<string> {
     const user = await userRepository.findByUsername(username);
     if (!user) throw new Error('Invalid credentials');
 
@@ -23,7 +25,7 @@ export class UserService {
     return jwt.sign({ sub: user.id }, secret, { expiresIn: '1h' });
   }
 
-  async getUser(id: number) {
+  async getUser(id: number): Promise<User | null> {
     return userRepository.findById(id);
   }
 }
